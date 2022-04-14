27d1d43f720dad0287f76c15fb6a44b4049a1a0e
zleeo01 <103614589+zleeo01@users.noreply.github.com>
2022-04-13 22:03:57 -0300

    adicionei o projeto agenda

---
+++ Agenda/src/main/java/controller/Controller.java
@@ -0,0 +1,242 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import model.DAO;
+import model.JavaBeans;
+
+// TODO: Auto-generated Javadoc
+//tesde de recebimento
+//System.out.println(request.getParameter("idcon"));
+//System.out.println(request.getParameter("nome"));
+//System.out.println(request.getParameter("fone"));
+//System.out.println(request.getParameter("email"));
+
+/**
+ * The Class Controller.
+ */
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert", "/select", "/update", "/delete", "/report" })
+public class Controller extends HttpServlet {
+	
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+	
+	/** The dao. */
+	DAO dao = new DAO();
+	
+	/** The contato. */
+	JavaBeans contato = new JavaBeans();
+
+	/**
+	 * Instantiates a new controller.
+	 */
+	public Controller() {
+		super();
+
+	}
+
+	/**
+	 * Do get.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		System.out.println(action);
+		if (action.equals("/main")) {
+			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
+		} else if (action.equals("/select")) {
+			listarContato(request, response);
+		} else if (action.equals("/update")) {
+			editarContato(request, response);
+		} else if (action.equals("/delete")) {
+			removerContato(request, response);
+		} else if (action.equals("/report")) {
+			gerarRelatorio(request, response);
+		} else {
+			response.sendRedirect("index.html");
+		}
+	}
+
+	/**
+	 * Contatos.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// listar contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// criando ujm objeto que ira receber os dados javabeans
+		ArrayList<JavaBeans> lista = dao.listarContatos();
+		// encaminhar a lista ao documento agenda.jsp
+		request.setAttribute("contatos", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("agenda.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * Novo contato.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// novo contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// setar as variaveis javabeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		// invocar o metodo inserircontato passando o objeto contato
+		dao.inserirContato(contato);
+		// redirecionar para o documento agenda.jsp
+		response.sendRedirect("main");
+	}
+
+	/**
+	 * Listar contato.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// editar contato
+	protected void listarContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// recebimento do id do contato q sera editado
+		String idcon = request.getParameter("idcon");
+		// setar variavel javabeans
+		contato.setIdcon(idcon);
+		// executar o metodod selecionarContato (dao)
+		dao.selecionarContato(contato);
+		// setar os atributos do formulario com o conteudo javabens
+		request.setAttribute("idcon", contato.getIdcon());
+		request.setAttribute("nome", contato.getNome());
+		request.setAttribute("fone", contato.getFone());
+		request.setAttribute("email", contato.getEmail());
+		// encaminhar ao documento editar.jsp
+		RequestDispatcher rd = request.getRequestDispatcher("Editar.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * Editar contato.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	protected void editarContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// setar as variaveis javabeans
+		contato.setIdcon(request.getParameter("idcon"));
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		// executar o metodo alterar contato
+		dao.alterarContato(contato);
+
+		// redirecionar para o doc agenda.jsp
+		response.sendRedirect("main");
+
+	}
+
+	/**
+	 * Remover contato.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// deletar contatos
+	protected void removerContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// recebimento do id do contato
+		String idcon = request.getParameter("idcon");
+		// setar variavel idcon javabeans
+		contato.setIdcon(idcon);
+		// executar o metodo deletar contato (dao) passando o objeto contato
+		dao.removerContato(contato);
+		// redirecionar para o doc agenda.jsp
+		response.sendRedirect("main");
+
+	}
+
+	/**
+	 * Gerar relatorio.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @throws ServletException the servlet exception
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	// gerar relatório em pdf
+	protected void gerarRelatorio(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Document documento = new Document();
+		try {
+			//tipo de conteudo
+			response.setContentType("apllication/pdf");
+			//nome do doc
+			response.addHeader("Content-Disposition", "inline; filename="+"contatos.pdf");
+			//criar doc
+			PdfWriter.getInstance(documento, response.getOutputStream());
+			//abrir doc > conteudo
+			documento.open();
+			documento.add(new Paragraph("Lista de contatos:"));
+			documento.add(new Paragraph(""));
+			//criar uma tabela
+			PdfPTable tabela = new PdfPTable(3);
+			//cabeçalho
+			PdfPCell col1 = new PdfPCell (new Paragraph("Nome"));
+			PdfPCell col2 = new PdfPCell (new Paragraph("Fone"));
+			PdfPCell col3 = new PdfPCell (new Paragraph("Email"));
+			tabela.addCell(col1);
+			tabela.addCell(col2);
+			tabela.addCell(col3);
+			//popular a tab com os contatos
+			ArrayList<JavaBeans> lista = dao.listarContatos();
+			for (int i=0; i<lista.size(); i++) {
+				tabela.addCell(lista.get(i).getNome());
+				tabela.addCell(lista.get(i).getFone());
+				tabela.addCell(lista.get(i).getEmail());
+			}
+			documento.add(tabela);
+			documento.close();
+		} catch (Exception e) {
+			System.out.println(e);
+			documento.close();
+		}
+	}
+
+}
---
+++ Agenda/src/main/webapp/scripts/confirmador1.js
@@ -0,0 +1,11 @@
+/**
+ * @author Rafael
+ * @param idcon
+ */
+
+function confirmar(idcon) {
+	let resposta = confirm("Confirma a exclusão deste contato?")
+	if (resposta === true) {
+		window.location.href = "delete?idcon=" + idcon
+	}
+}
